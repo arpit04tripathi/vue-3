c498cdc7b96a51811d62e77dad352fa24f9823a7
Arpit Tripathi <arpit04tripathi@gmail.com>
2025-09-18 23:00:14 +0100

    TestBootstrapIcons

---
+++ src/views/Bootstrap.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>This is bootstrap view</h1>
+        <TestBootstrapIcons />
+    </div>
+</template>
+
+<script setup lang="ts">
+import TestBootstrapIcons from '../components/test/TestBootstrapIcons.vue'
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
