f95e30463d641f0598643c125d2a98e8eef1b998
Arpit Tripathi <arpit04tripathi@gmail.com>
2025-09-20 15:23:27 +0100

    header

--- src/components/_layout/HeaderComponent.vue
+++ src/components/_layout/HeaderComponent.vue
@@ -31,6 +31,25 @@
             <li class="nav-item">
               <RouterLink class="nav-link p-2" to="/default">Default</RouterLink>
             </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="https://vuejs.org/examples/#hello-world" target="_blank"
+                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Examples
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <RouterLink class="nav-link p-2" to="/about">About</RouterLink>
+                </li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
           </ul>
         </div>
 
